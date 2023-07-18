e917f893f60693458266783b23f34a64ee815b06
Dingteng <dingtengdingteng@163.com>
2023-07-18 17:14:40 +0800

    change title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          市场监管教育在线AI大模型
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          连接知识，传递价值。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
