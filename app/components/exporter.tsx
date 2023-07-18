9ff517a9300d2fdb458d3c9251b04c6bd76a2e3c
Dingteng <dingtengdingteng@163.com>
2023-07-18 16:35:41 +0800

    change logo

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -16,7 +16,7 @@ import { copyToClipboard, downloadAs, useMobileScreen } from "../utils";
 
 import CopyIcon from "../icons/copy.svg";
 import LoadingIcon from "../icons/three-dots.svg";
-import ChatGptIcon from "../icons/chatgpt.png";
+import ChatGptIcon from "../icons/chatgpt-new.png";
 import ShareIcon from "../icons/share.svg";
 import BotIcon from "../icons/bot.png";
 
